f854226a2e59cce6e49fa40751a3e4f8967fb44a
Joyce Lee <shuwei1203@gmail.com>
2025-04-07 22:20:58 +0100

    added edit button

--- fitlog-site/components/fitlog.tsx
+++ fitlog-site/components/fitlog.tsx
@@ -98,23 +98,34 @@ const Fitlog: React.FC = () => {
                 <div className="bg-white p-4 rounded-xl shadow-lg relative my-3 w-full">
                   <button
                     // onClick={handleAddWorkout}
-                    className="absolute top-2 right-2 text-white bg-sky-600 hover:bg-sky-700 w-8 h-8 flex items-center justify-center rounded-full shadow"
+                    className="absolute top-4 right-4 text-white bg-sky-600 hover:bg-sky-700 w-8 h-8 flex items-center justify-center rounded-full shadow"
                   >
                     +
                   </button>
 
-                  <div className="mt-8 space-y-2">
+                  <div className="mt-10 space-y-2">
                     {workouts.length > 0 ? (
                       workouts.map((log, index) => (
-                        <div key={index} className="p-2 bg-sky-700 rounded-md shadow-sm text-white text-left">
-                          <div>
-                            {log.name}
+                        <div key={index} className="px-2 text-left">
+                          <div className="flex justify-between items-center">
+                            <div>
+                              <div>
+                                {log.name}
+                              </div>
+                              <div className="flex space-x-4">
+                                <span>{log.weight}kg</span>
+                                <span>{log.reps} reps</span>
+                                <span>{log.sets} sets</span>
+                              </div>
+                            </div>
+                            <button
+                              // onClick={() => handleEdit(index)}
+                              className="text-blue-500 hover:text-blue-700"
+                            >
+                              edit &gt;
+                            </button>
                           </div>
-                          <div className="flex space-x-4">
-                            <span>{log.weight}kg</span>
-                            <span>{log.reps} reps</span>
-                            <span>{log.sets} sets</span>
-                          </div>
+                          <div className="border-b border-gray-300 my-3"></div>
                         </div>
                       ))
                     ) : (
