a48fef7dfcb2e37aaecd8ca258a59b5117788d47
Joyce Lee <shuwei1203@gmail.com>
2025-04-12 22:58:10 +0100

    added index true to id in database

--- app/models.py
+++ app/models.py
@@ -8,16 +8,16 @@ Base = declarative_base()
 class WorkoutType(Base):
 	__tablename__ = 'workout_types'
 
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True, index=True)
 	name = Column(String, nullable=False)
-	color = Column(String, nullable=True)
+	color = Column(String, nullable=False)
 
 	sessions = relationship("WorkoutSession", back_populates="workout_type")
 
 class WorkoutSession(Base):
 	__tablename__ = 'workout_sessions'
 
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True, index=True)
 	date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 	workout_type_id = Column(Integer, ForeignKey('workout_types.id'))
@@ -28,7 +28,7 @@ class WorkoutSession(Base):
 class Workout(Base):
 	__tablename__ = 'workouts'
 
-	id = Column(Integer, primary_key=True)
+	id = Column(Integer, primary_key=True, index=True)
 	name = Column(String, nullable=False)
 	reps = Column(Integer, nullable=False)
 	weight = Column(Integer, nullable=False)
